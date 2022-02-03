26821820a6a4c8d804ce143b430783f4e91b020b
justSevbo <sevbofx@gmail.com>
2022-02-03 12:35:18 +0500

    models.py and admin.py Updated

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Post, Category
+
+
+admin.site.register(Category)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at', 'active','views')
+    list_filter = ('updated_at', 'active', 'category', 'views')
+    # prepopulated_fields = {'slug': ('title',)}
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,33 @@
+from distutils.command.upload import upload
+from statistics import mode
+from django import views
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    category = models.CharField(max_length=30)
+    category_image = models.ImageField(upload_to='category_images')
+
+    def __str__(self) -> str:
+        return self.category
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=210)
+    # slug = models.SlugField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    text = models.TextField()
+    active = models.BooleanField(default=True)
+    comments = models.IntegerField(default=0)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+    images = models.ImageField(upload_to='post_images')
+    category = models.ManyToManyField(Category)
+
+    class Meta:
+        ordering = ('created_at', )
+
+    def __str__(self) -> str:
+        return self.title
