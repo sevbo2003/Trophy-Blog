595ccef47e82aca27058de485492ca7698b95606
justSevbo <sevbofx@gmail.com>
2022-02-03 14:25:51 +0500

    urls and views file added

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
+from multiprocessing import context
+from tokenize import Triple
 from django.shortcuts import render
+from .models import Category, Post
+
 
 # Create your views here.
+def post_list(request):
+    posts = Post.objects.filter(active=True)
+    context = {'posts': posts}
+    return render(request, 'home.html', context)
