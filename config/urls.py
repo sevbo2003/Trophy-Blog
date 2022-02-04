db16e002baf96af82c87daed4dae0fdcad960f6d
justSevbo <sevbofx@gmail.com>
2022-02-04 23:06:34 +0500

    Final commit

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('malikov/admin', admin.site.urls),
+    path('malikov/admin/', admin.site.urls),
     path('', include('blog.urls'))
 ]
